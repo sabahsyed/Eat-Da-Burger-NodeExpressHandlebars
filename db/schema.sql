7f1a0c7b735485cd65bd70e0075c9746bb515f73
Sabah Syed <sabisyed@gmail.com>
2020-05-26 23:33:01 -0700

    Initial Commit-All folder paths nad dafatbse created

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+Create database burgers_db;
+USE burgers_db;
+
+create table burgers(
+    id
+    burger_name
+    devoured
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert INTO burgers (burger_name,devoured) values ("Chicken burger",true);
+insert INTO burgers (burger_name,devoured) values ("Zinger burger",false);
+insert INTO burgers (burger_name,devoured) values ("Beef burger",true);
+insert INTO burgers (burger_name,devoured) values ("Chicken hot burger",false);
