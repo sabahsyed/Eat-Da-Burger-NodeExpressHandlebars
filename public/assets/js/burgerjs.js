b99c36ead7db4a6dc937fd0423f2fc58784f2590
Sabah Syed <sabisyed@gmail.com>
2020-05-28 23:58:04 -0700

    Check

--- public/assets/js/burgerjs.js
+++ public/assets/js/burgerjs.js
@@ -1,7 +1,7 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
     $("#devour").on("click", function(event) {
-      var id = $(this).data("id");
+      var id = $("burger_id").data("id");
   
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
